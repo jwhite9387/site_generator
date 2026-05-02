2b89caba4a611d274f4090164c5c7db0d7453f84
jwhite9387 <jwhite93@uab.edu>
2026-05-02 02:15:17 -0500

    tests updated

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -29,3 +29,14 @@ class TestHTMLNode(unittest.TestCase):
     def test_leaf_to_html_p(self):
         node = LeafNode("p", "Hello, world!")
         self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        self.assertNotEqual(node.to_html(), "<P>Hello, world!</P>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click Me!", {"href": "https://google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://google.com">Click Me!</a>')
+        self.assertNotEqual(node.to_html(), '<a href="https://google.com">Click Me?</a>')
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "This is bold text.")
+        self.assertEqual(node.to_html(), "<b>This is bold text.</b>")
+        self.assertNotEqual(node.to_html(), "<b>This is italic text.</b>")
