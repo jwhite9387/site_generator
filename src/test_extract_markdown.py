13454001a6c5ea4e89482b3b62a9666f29e1ff64
jwhite9387 <jwhite93@uab.edu>
2026-05-12 23:31:31 -0500

    markdown extraction function and test files made

---
+++ src/test_extract_markdown.py
@@ -0,0 +1,20 @@
+import unittest
+
+from extract_markdown import extract_markdown_images, extract_markdown_links
+
+
+class TestExtractMarkdown(unittest.TestCase):
+
+    def test_extract_markdown_images(self):
+        matches = extract_markdown_images(
+            "This is a test with an ![image](https://i.imgur.com/zjjcJKZ.png)"
+        )
+
+        self.assertListEqual([("image", "https://i.imgur.com/zjjcJKZ.png")], matches)
+
+    def test_extract_markdown_links(self):
+        matches = extract_markdown_links(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+        )
+
+        self.assertListEqual([("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")], matches)
