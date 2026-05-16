33c6d33028e922415c2236989429c46a2fbe8809
jwhite9387 <jwhite93@uab.edu>
2026-05-16 01:02:30 -0500

    text to textnodes and test files done

---
+++ src/text_to_textnodes.py
@@ -0,0 +1,15 @@
+from textnode import *
+from split_delimiter import *
+from split_markdown import *
+from extract_markdown import *
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextType.TEXT)]
+    nodes = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "_", TextType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextType.CODE)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+
+    return nodes
