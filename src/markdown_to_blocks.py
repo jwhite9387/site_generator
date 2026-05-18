5bb81b7f6b3b6b6cae40c4adcec3b5579ab5bbf0
jwhite9387 <jwhite93@uab.edu>
2026-05-17 19:47:21 -0500

    markdown to blocks and test files

---
+++ src/markdown_to_blocks.py
@@ -0,0 +1,9 @@
+def markdown_to_blocks(markdown):
+    blocks = markdown.split("\n\n")
+    new_blocks = []
+    for block in blocks:
+        new_block = block.strip()
+        if new_block == "":
+            continue
+        new_blocks.append(new_block)
+    return new_blocks
